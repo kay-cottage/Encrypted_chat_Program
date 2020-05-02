807ce0b087cb9c89c4658046784b86395a0dcb37
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2020-05-03 01:17:30 +0800

    Encrypted_chat_Program

--- udp_x64.py
+++ udp_x64.py
@@ -4,6 +4,9 @@ from socket import *
 import getpass
 from threading import Thread
 import hashlib
+
+
+#接受消息
 def rec_data():
   while True:
     rec_info = udpsocket.recv(1024).decode()
@@ -11,6 +14,8 @@ def rec_data():
     d = decrypt(rec_info,key)
     print("\r>>%s:%s" % (ip_ad, d))
     print(" ", end="")
+
+#发送消息
 def send_date():
   while True:
     text = input("<<")
@@ -20,6 +25,8 @@ def send_date():
 udpsocket = None
 desip = ""
 desport = 0
+
+#消息取段处理
 def add_to_16(text):
     if len(text.encode('utf-8')) % 16:
         add = 16 - (len(text.encode('utf-8')) % 16)
@@ -31,18 +38,15 @@ def add_to_16(text):
 
 # 加密函数
 def encrypt(text,key):
-    #key = '9999999999999999'.encode('utf-8')
     mode = AES.MODE_ECB
     text = add_to_16(text)
     cryptos = AES.new(key, mode)
-
     cipher_text = cryptos.encrypt(text)
     return b2a_hex(cipher_text)
 
 
 # 解密后，去掉补足的空格用strip() 去掉
 def decrypt(rec_info,key):
-    #key = '9999999999999999'.encode('utf-8')
     mode = AES.MODE_ECB
     cryptor = AES.new(key, mode)
     plain_text = cryptor.decrypt(a2b_hex(rec_info))
@@ -55,30 +59,31 @@ def main():
   global key
   print('本程序为加密聊天程序')
   print('<<为可发送状态')
-  print('xxxxxx<<表示好友消息')
   print(' ')
   locport = int(input('请绑定本地端口（默认33333）：'))
   desip = input("远程IP：")
   desport = int(input("对方端口（默认33333）："))
   print('以下请正确输入与好友约定的密钥')
-  print('该密钥几乎可以是任意定长的字符串！（例如一段文字，甚至可以是一整本书的文字）')
+  print('该密钥可以是几乎任意定长的字符串！')
+  
+  #两次SHA生成密钥。此部分可自定义修改，例如可以使用RSA生成密钥key
   a = input('请输入该密钥：').encode("utf8")
   b = hashlib.sha256(a).hexdigest()
   c = hashlib.sha256(b.encode("utf8")).hexdigest()
-  d = hashlib.sha256(c.encode("utf8")).hexdigest()
-  key = d[0:32].encode('utf8')
-  #print(key)
+  key = d[6:38].encode('utf8')
   udpsocket = socket(AF_INET, SOCK_DGRAM)
   udpsocket.bind(("", locport))
-  print('##########################已经成功创建socket#####################')
+  print('######################已经成功创建socket#####################')
   for i in range(60):
                 print('                          >>>>>>><<<<<<<<                                     ')
-  print('***************************可以开始会话***************************')           
+  print('***********************可以开始会话***************************')           
   tr = Thread(target=rec_data)
   ts = Thread(target=send_date)
   tr.start()
   ts.start()
   tr.join()
   ts.join()
+
+  
 if __name__ == '__main__':
   main()
